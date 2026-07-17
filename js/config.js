9b90df9fc715f3f20287ee7d6f692751a4dde779
DING Ruiqi <112251998+Ricky-Ding@users.noreply.github.com>
2026-07-17 16:29:57 +0800

    config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,8 @@
+// ============ 配置文件 ============
+// 修改这里的 API Key 和 Bin ID
 
+const CONFIG = {
+    apiKey: '你的JSONBin_API_Key',   // ← 改成你的
+    binId: '你的Bin_ID',             // ← 改成你的
+    storageKey: 'travel_memories'
+};
