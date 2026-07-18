2ba1d1a62a20c9b4408d58c0aebda4f4d2c4d7ff
DING Ruiqi <112251998+Ricky-Ding@users.noreply.github.com>
2026-07-18 12:00:04 +0800

    config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 const CONFIG = {
     apiKey: '$2a$10$F86ANUc7v/rvTzIIDo3HKOzBEsjNvWirQGPAZqlf6PuS99QJ1X4om',
     binId: '6a59dd0ada38895dfe69b8f9',
-    storageKey: 'travel_memories'
+    storageKey: 'travel_memories',
+    password: '123456'
 };
 
 const IMGBB_CONFIG = {
